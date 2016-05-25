5d823c32a201d1562dd07d7465c10ab7ddee6503
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-05-25 14:32:25 +0900

    5の倍数

--- lib/FizzBuzz.php
+++ lib/FizzBuzz.php
@@ -13,7 +13,7 @@ class FizzBuzz
     if ( $n === 1 ) {
       return '1';
     }
-    if ( $n === 5 ) {
+    if ( $n % 5 === 0 ) {
       return 'Buzz';
     }
   }
--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -14,14 +14,16 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('Fizz', FizzBuzz::check(6));
   }
 
-  public function test_5入れたらFizz()
+  public function test_5の倍数を入れたらFizz()
   {
     $this->assertEquals('Buzz', FizzBuzz::check(5));
+    $this->assertEquals('Buzz', FizzBuzz::check(10));
   }
 
-  public function test_15入れたらFizzBuzz()
+  public function test_15の倍数を入れたらFizzBuzz()
   {
     $this->assertEquals('FizzBuzz', FizzBuzz::check(15));
+    $this->assertEquals('FizzBuzz', FizzBuzz::check(30));
   }
 
 }
