05320cbf72794606d3de99ebed4e7e3afd4ea3e4
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2016-06-22 14:37:58 +0900

    いらないテスト削除

--- test/CommandTest.php
+++ test/CommandTest.php
@@ -48,22 +48,6 @@ class CommandTest extends PHPUnit_Framework_TestCase
 
     $this->assertEquals([], $spy->result());
   }
-/*
-  public function test_1を実行せずに2を入力すると何も表示されない()
-  {
-  }
-*/
-
-  public function test_1を実行してからに2を入力すると履歴が表示される()
-  {
-    $spy = new StdoutSpy();
-    $logger = new Logger();
-    $command = new Command(null, $spy, $logger);
-    $logger->add('3: Fizz');
-    $command->run('2');
-
-    $this->assertEquals(['3: Fizz'], $spy->result());
-  }
 
   public function test_1を複数回実行してからに2を入力するとすべての履歴が表示される()
   {
